b6b37b17b40efd5e96a0c8617b92632900b43a14
Valorie Zhang <valoryxiaowen@gmail.com>
2019-11-10 15:46:34 -0500

    portfolio1010

--- portfolio/js/javascript.js
+++ portfolio/js/javascript.js
@@ -57,15 +57,8 @@ jQuery(document).ready(function() {
   });
 });
 
-// /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-// function myFunction() {
-//   var x = document.getElementById("mainNav");
-//   if (x.className === "topnav") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "topnav";
-//   }
-// }
+
+
 
 
 
